8dd3902f3df84ed479924611c9db70ecfe7aaf50
Aaron Trierweiler <ait530@users.noreply.github.com>
2017-04-19 18:30:05 -0500

    table now lists items properly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -29,6 +29,7 @@ var connection = mySql.createConnection({
 // });
 
 connection.query("SELECT * FROM products", function(err, res) {
+  console.log("--------------------------------------------");  
   for (var i = 0; i < res.length; i++) {
     console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price) + " | " + res[i].stock_quantity;
   }
