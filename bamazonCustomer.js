39940715cbd16d169dfe23f861d35ed9e9b9241f
Aaron I Trierweiler <aaronitrierweiler@MacBook-Pro.local>
2017-04-21 11:08:02 -0500

    updated pseudocode

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -29,7 +29,7 @@ var connection = mySql.createConnection({
 // });
 
 connection.query("SELECT * FROM products", function(err, res) {
-  console.log("--------------------------------------------");  
+  console.log("--------------------------------------------");
   for (var i = 0; i < res.length; i++) {
     console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price) + " | " + res[i].stock_quantity;
   }
@@ -38,7 +38,19 @@ connection.query("SELECT * FROM products", function(err, res) {
 
 
 
+
+// To Do:
 // 'node bamazonCustomer.js' should do the following:
 // 1) Display all of the items available for sale (ids, names, and prices of products for sale)
 // 2) Prompts users with 2 messages
+// Uses github and heroku process
+// Includes screenshots and/or a video showing the app working w/no bugs (included in README file)
+
+
+// Completed:
+// Created MySQL database, Bamazon.
+// Created table called products in Bamazon
+// Table includes the following columns: item_id (unique id for each product) product_name (Name of product) department_nameprice (cost to customer) stock_quantity (how much of the product is available in stores)
+// Data base has 10 mock data rows
+// Created node app called bamazonCustomer.js
 // 
